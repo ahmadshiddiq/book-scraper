20492e1887df1cbb469e12e6555b950856d044c0
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-09-03 16:01:54 +0700

    chore: get data in new page

--- pageScraper.js
+++ pageScraper.js
@@ -18,7 +18,52 @@ const scraperObject = {
       return links;
     });
 
-    console.log(urls);
+    // loop through each of those links, open a new page instance and get relevant data from it
+    let pagePromise = (link) =>
+      new Promise(async (resolve, reject) => {
+        let dataObj = {};
+        let newPage = await browser.newPage();
+        await newPage.goto(link);
+        dataObj["bookTitle"] = await newPage.$eval(
+          ".product_main > h1",
+          (text) => text.textContent
+        );
+        dataObj["bookPrice"] = await newPage.$eval(
+          ".price_color",
+          (text) => text.textContent
+        );
+        dataObj["noAvailable"] = await newPage.$eval(
+          ".instock.availability",
+          (text) => {
+            // strip new line and tab spaces
+            text = text.textContent.replace(/(\r\n\t|\n|\r|\t)/gm, "");
+            // get the number of stock available
+            let regexp = /^.*\((.*)\).*$/i;
+            let stockAvailable = regexp.exec(text)[1].split(" ")[0];
+            return stockAvailable;
+          }
+        );
+        dataObj["imageUrl"] = await newPage.$eval(
+          "#product_gallery img",
+          (img) => img.src
+        );
+        dataObj["bookDescription"] = await newPage.$eval(
+          "#product_description",
+          (div) => div.nextSibling.nextSibling.textContent
+        );
+        dataObj["upc"] = await newPage.$eval(
+          ".table.table-striped > tbody > tr > td",
+          (table) => table.textContent
+        );
+        resolve(dataObj);
+        await newPage.close();
+      });
+
+    for (link in urls) {
+      let currentPageData = await pagePromise(urls[link]);
+      // scrapedData.push(currentData);
+      console.log(currentPageData);
+    }
   },
 };
 
